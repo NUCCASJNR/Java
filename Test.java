2d0dbac9a74a01a95a6e496cd06c38c36e4ffc21
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-02-21 09:48:13 +0100

    learnt about local var

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public void alareefAge() {
+       int age = 0;
+       age = age + 17;
+       System.out.println("Alareef's age is : " + age);
+    }
+ 
+    public static void main(String args[]) {
+       Test test = new Test();
+       test.alareefAge();
+    }
+ }
